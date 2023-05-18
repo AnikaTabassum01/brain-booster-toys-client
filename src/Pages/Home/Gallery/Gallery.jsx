a323735c97d4ee6ef4305750dec8c8f86ef43a4e
Anika <anika.bd1994@gmail.com>
2023-05-18 16:11:34 +0600

    basic stracture made

---
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div>
+            <h2>
+                Gallery Part here
+            </h2>
+        </div>
+    );
+};
+
+export default Gallery;
